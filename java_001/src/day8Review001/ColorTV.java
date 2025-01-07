09aa92e21a112ff4ef304dae4bc43fccb0678927
공주 <@yul>
2025-01-08 00:15:44 +0900

    2025.01.07 Day8 상속 Test

--- java_001/src/day8Review001/ColorTV.java
+++ java_001/src/day8Review001/ColorTV.java
@@ -2,25 +2,21 @@ package day8Review001;
 
 public class ColorTV extends TV{ //자식클래스
 
+	public static void main(String[] args) {
 		
 		//다음 main() 메소드와 실행 결과를 참고하여 TV를 상속받은 ColorTV 클래스를 작성하라.
 		//결과 : 32인치 1024컬러
-	public static void main(String[] args) {
+		ColorTV myTV = new ColorTV(32, 1024);
+		myTV.printProperty();
+	}
 
 		private int color;
-		
+		ColorTV(int size, int color){
+			super(size);
+			this.color = color;
+			System.out.println(size + "인치" + color + "컬러");		
 	
-		   ColorTV myTV = new ColorTV(32, 1024);
-		   super(size);
-		   this.color = color;
-	}
-	public void printProperty() {
+
+	}   
 		
-		System.out.println(getSize() + "인치" + color + "컬러");
-	}
 }
-
-	
-		   
-
-
