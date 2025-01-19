d070bcaeb9b817a58ba1126f46cab9e1a1d52be6
USER <USER@DESKTOP-CL9FL0L>
2025-01-20 02:06:18 +0900

    2025.01.20 DB 주석

--- java_001/src/day16/DBInsertEx1.java
+++ java_001/src/day16/DBInsertEx1.java
@@ -21,6 +21,9 @@ public class DBInsertEx1 {
 		// TODO Auto-generated method stub
 		Connection conn;
 		Statement stmt = null;
+		//Statement 초기값이 null인 이유
+		//Steatement는 SQL문을 실행하기 위해 사용됨.
+		//db연결이 생성된 이후에만 초기화 할 수 있음. 그래서 db연결 전 선언만 하고 null로 설정
 		Scanner scan = new Scanner(System.in);
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver"); 
@@ -29,7 +32,7 @@ public class DBInsertEx1 {
 			String db_pw = "TEST1234";
 			conn = DriverManager.getConnection(db_url, db_id, db_pw);
 			System.out.println("DB 연결 완료");
-			stmt = conn.createStatement();
+			stmt = conn.createStatement(); //실제 Statement 생성
 			// 학번(String - 8글자), 이름, 학과를 입력받아서
 			// STUDENT 테이블에 저장
 			System.out.print("학번 : ");
@@ -39,10 +42,11 @@ public class DBInsertEx1 {
 			System.out.print("학과 : ");
 			String dept = scan.next();
 			
-			String sql 
+			String sql // INSERT, UPDATE, DELETE를 작성하기 위해 사용.
 				= "INSERT INTO STUDENT(STU_NO, STU_NAME, STU_DEPT) "
 				+ "VALUES('" + stuNo + "', '" + name + "', '" + dept + "')";
 			int num = stmt.executeUpdate(sql);
+			//SQL은 실행결과로 변경된 행(ROW)의 개수가 계산됨. ROW의 개수를 반환
 			if(num > 0) {
 				System.out.println("추가되었음!");
 			} else {
--- java_001/src/day16/StudentManager.java
+++ java_001/src/day16/StudentManager.java
@@ -9,12 +9,22 @@ public class StudentManager {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
+			//sql은 SELECT SQL문자열임.
+			//ResultSet (rs) = 저장. SELECT 결과를 테이블 형태로 저장.
+			//rs.next():사용으로 행row를 이동.
+			//각 열(column)의 데이터를 rs.getString, rs.getInt 등으로 읽음(ㄱㅏ져옴)
+			//PrintData = SELECT결과를 출력
+			//(\t)탭. 홍길동170 ==> 홍길동  170
+			
 			Scanner scan = new Scanner(System.in);
 			DBClass db = new DBClass();
 			Statement stmt = db.stmt; 
+			//Statement는 SQL문을 실행하기위해 사용됨
+			//DB연결이 생성된 이후에만 초기화 할 수 있따.
 			String colKor[] = {"이름", "학과", "키"};
+			//사용자에게 보여줄 한글이름.
 			String colEng[] = {"STU_NAME", "STU_DEPT", "STU_HEIGHT"};
-			
+			//DB에서 사용 할 실제 컬럼이름.
 			while(true) {
 				System.out.print("[(1) 학생추가 (2) 수정 (3) 삭제 (4) 조회 (5) 종료] : ");
 				int menu = scan.nextInt();
@@ -37,6 +47,9 @@ public class StudentManager {
 						= "INSERT INTO STUDENT(STU_NO, STU_NAME, STU_DEPT) "
 						+ "VALUES('" + stuNo + "', '" + name + "', '" + dept + "')";
 					int num = stmt.executeUpdate(sql);
+					//executeUpdate = 반환값 정수형 int
+					//JDBC에서 DB의 데이터 변경작업 (INSERT, UPDATE, DELETE)를 수행하는 메소드
+					//행을 추출.
 					if(num > 0) {
 						System.out.println("추가되었음!");
 					} else {
@@ -46,15 +59,24 @@ public class StudentManager {
 					System.out.print("학번 : ");
 					String stuNo = scan.next();
 					ResultSet rs =  stmt.executeQuery("SELECT * FROM STUDENT WHERE STU_NO = '" + stuNo + "'");
+					//executeQuery = DB에 저장된 값을 가져옴.
+					//JDBC에서 SELECT문을 실행하기 위한 메소드.
+					//SQL SELECT를 실행하고, 결과를 RESELTSET 객체에 담아 반환한다.
+					//DB조회작업에 사용. **DB의 저장값을 가져옴**
 					if(rs.next()) {
+						//rs = SQL SELECT 결과를 담은 객체
+						//주로 stastment or preparedStatement를 통해 생성
+						//rs.next는 ResultSet의 다음행으로 이동. **데이터를 읽으려면**
 						System.out.println("수정할 데이터 선택 ");
 						System.out.print("(1) 이름 (2) 학과 (3) 키 => " );
 						int column = scan.nextInt();
 						if(column > 3 || column < 1) {
+							//컬럼이 1~3까지 존재하므로, 1~3이 아닌 값을 입력하면 없는정보 반환.
 							System.out.println("없는 정보입니다.");
 							continue;
 						}
 						System.out.print(colKor[column-1] + " 입력 : ");
+						//자바에서 배열은 0부터 시작하기때문에 -1을 해줌.
 						String value = scan.next();
 						stmt.executeUpdate("UPDATE STUDENT SET " + colEng[column-1] + " = '" + value + "' " + "WHERE STU_NO = '" + stuNo + "'");
 						System.out.println("수정되었습니다.");
